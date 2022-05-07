645c0371f35831313b9f13204e43e98b2d1b193e
AlinaGolkina <67148494+AlinaGolkina@users.noreply.github.com>
2022-05-08 01:22:53 +0300

    HA6_SOLID

--- Flask_app/SD_HA4_2_messenger.py
+++ Flask_app/SD_HA4_2_messenger.py
@@ -1,16 +1,16 @@
-# Section 2. Classes
-# Var2 (a messenger):
-# Compare messengers by users (or chats, or messages) count (A > B = False)
-# Get messengers unique users difference/union/intersection by phone (or email address) (A - B = {'+790324...', ...}
+#SOLID Principles on classes
 
 class user():
-    def __init__(self, login, phone, gender, age, messages=list, chats=list):
+    def __init__(self, login, phone, gender, age):
         self.__login = login
         self.__phone = phone
         self.__gender = gender
         self.__age = age
-        self.messages = messages
-        self.chats = chats
+        self.messages = []
+        self.chats = []
+    
+    def __repr__(self):
+        return self.login
         
     @property
     def login(self):
@@ -59,7 +59,75 @@ class user():
     @age.deleter
     def age(self):
         del self.__age
+        
 
+class message():
+    def __init__(self, user, messages):
+        self.__user = user
+        self.__messages = messages
+        
+    @property
+    def user(self):
+        return self.__user
+    
+    @user.setter
+    def user(self, user: user):
+        self.__user = user
+        
+    @user.deleter
+    def user(self, name):
+        del self.__user
+        
+    @property
+    def messages(self):
+        return self.__messages
+    
+    @messages.setter
+    def messages(self, messages):
+        self.__user = messages
+        
+    @messages.deleter
+    def messages(self, messages):
+        del self.__messages
+        
+        
+class chat():
+    def __init__(self, name, date):
+        self.__name = name
+        self.__date = date
+        self.details = {}
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name):
+        self.__name = name
+        
+    @name.deleter
+    def name(self, name):
+        del self.__login
+        
+    @property
+    def date(self):
+        return self.__date
+    
+    @date.setter
+    def date(self, date):
+        self.__date = date
+        
+    @date.deleter
+    def date(self, date):
+        del self.__date
+        
+    def add_users(self, user: user):
+        self.details[user] = None
+    
+    def add_message(self, message: message):
+        self.details[message.user] = message.messages
+        message.user.messages.append(message.messages)
+        
         
 class messenger():
     def __init__(self):
@@ -69,11 +137,11 @@ class messenger():
         self.messages = dict()
         self.chats = dict()
         
-    def add_user(self, user):
+    def add_user(self, user: user, chat: chat):
         self.users[user] = user.login
         self.phones[user] = user.phone
         self.messages[user] = user.messages
-        self.chats[user] = user.chats
+        self.chats[user] = chat.name
             
     # Find messages with some word combination
     def find_message(self, key_word: str):
@@ -106,24 +174,14 @@ class messenger():
     
     def union(self, other):
         return set(self.phones.values()) | set(other.phones.values())
- 
-# create users
-user1 = user('Elena','7916000','female','18',['ok','no'])
-user2 = user('Marina','7925000','female','20',['ok'])
-user3 = user('Alina','7928000','female','30',['yes'])
-user4 = user('Ekaterina','7925000','female','35',['ok'])
+    
+    
+user1 = user('Elena','7916000','female','21')
+chat1 = chat('chat1','2022-01-01')
+chat1.add_users(user1)
 
-# create first messenger
-mes1 = messenger()
-mes1.add_user(user1)
-mes1.add_user(user2)
-
-# create second messenger
-mes2 = messenger()
-mes2.add_user(user3)
-mes2.add_user(user4)
-mes2.add_user(user1)
-
-print('finding messages with key_word' ,mes1.find_message('ok'))
-print(f'phones union, {mes1.union(mes2)}')
-print(f'phones intersection, {mes1.intersection(mes2)}')
+chat1.details
+mes1 = message(user1, 'hello')
+chat1.add_message(mes1)
+chat1.details
+user1.messages
